fe5d9c3945f511924d0581df08fe1564f945edb7
esharifu <esharifu@redhat.com>
2019-01-24 13:31:19 +0100

    feat: Add TreeProvider

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@ import { promise as ping } from 'ping';
 import { ObservableStorage } from './ObservableStorage';
 import { Pinger } from './Pinger';
 import { UIController } from './UIController';
+import { PingTreeProvider } from './PingTreeProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -12,6 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
     const storage = new ObservableStorage<ping.PingResponse>();
     const pinger = new Pinger(storage);
     const uiController = new UIController(pinger);
+    const treeProvider = new PingTreeProvider(storage);
 
     // registerCommand tells VSCode to use this callback for certain command
     // and creates disposable.
@@ -19,6 +21,11 @@ export function activate(context: vscode.ExtensionContext) {
         // This handler running any time command is called.
         await uiController.processPingCommand();
     });
+
+    // registering TreeProvider with name pings.
+    // We need to declare tree providers in package.json.
+    // VSCode will search for providers with names declared in package.json.
+    const registeredTree = vscode.window.registerTreeDataProvider('pings', treeProvider);
 
     // Creating status bar item and adding it to status bar.
     const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
@@ -30,6 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
     // Telling VSCode to use this disposables to release resources
     context.subscriptions.push(statusBarItem);
     context.subscriptions.push(registeredCommand);
+    context.subscriptions.push(registeredTree);
 }
 
 export function deactivate() { }
